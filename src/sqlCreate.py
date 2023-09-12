34e50cae707599ed84a0a98b2e57b30f17b3315f
AccessAndrei <116093528+AccessAndrei@users.noreply.github.com>
2023-09-13 02:40:13 +0300

    1

--- src/sqlCreate.py
+++ src/sqlCreate.py
@@ -3,7 +3,7 @@ import sqlite3 as sq
 
 class DataBase:
     def __init__(self):
-        with sq.connect("priemka.db") as con:
+        with sq.connect("../database/priemka.db") as con:
             self.cur = con.cursor()
             self.cur.execute("""CREATE TABLE IF NOT EXISTS users(
             id INTEGER,
