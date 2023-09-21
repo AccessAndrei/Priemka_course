833a1ee4684df8ea77577b4f595cee5c68c90d13
AccessAndrei <116093528+AccessAndrei@users.noreply.github.com>
2023-09-22 00:57:38 +0300

    registration and log_in

--- src/sqlCreate.py
+++ src/sqlCreate.py
@@ -6,8 +6,58 @@ class DataBase:
         with sq.connect("../database/priemka.db") as con:
             self.cur = con.cursor()
             self.cur.execute("""CREATE TABLE IF NOT EXISTS users(
-            id INTEGER,
+            id INTEGER PRIMARY KEY AUTOINCREMENT ,
+            login TEXT,
             fullname TEXT,
-            status TEXT
+            status TEXT,
+            password TEXT
             )""")
+
+    @staticmethod
+    def registration():
+        login = input('login:\n')
+        name = input('fullname:\n')
+        status = input('Должность:\n')
+        password = input('Пароль:\n')
+        try:
+            db = sq.connect('../database/priemka.db')
+            cursor = db.cursor()
+            cursor.execute("""
+            SELECT login FROM users WHERE login = ?
+            """, [login])
+            if cursor.fetchone() is None:
+                cursor.execute("INSERT INTO users(login, fullname, status, password) VALUES (?,?,?,?)", [login, name,status, password])
+                db.commit()
+            else:
+                print("Same login exists")
+                DataBase.registration()
+        except sq.Error as e:
+            print(e)
+        finally:
+            cursor.close()
+            db.close()
+
+
+    @staticmethod
+    def log_in():
+        login = input("login:\n")
+        password = input('password:\n')
+        try:
+            db = sq.connect('../database/priemka.db')
+            cursor = db.cursor()
+            cursor.execute("SELECT login, password FROM users WHERE login = ? AND password = ?", [login, password])
+            if (login, password) in cursor.fetchall():
+                print('success')
+            else:
+                print("неверный логин или пароль")
+
+        except sq.Error as e:
+            print(e)
+        finally:
+            cursor.close()
+            db.close()
+
+
 data = DataBase()
+#DataBase.registration()
+DataBase.log_in()
---
+++ src/working_with_tables.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+with sqlite3.connect('../database/priemka.db') as db:
+    cursor = db.cursor()
+    cursor.execute("DROP TABLE IF EXISTS sqlite_schema")
