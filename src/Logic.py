33229af8e0d414a83177697d4781abf764229699
AccessAndrei <116093528+AccessAndrei@users.noreply.github.com>
2023-10-06 22:13:19 +0300

    feat: auth been modernised

--- src/Logic.py
+++ src/Logic.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import sqlite3 as sq
 from tkinter import messagebox
 
+
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -46,6 +47,7 @@ class Application(tk.Tk):
                 print('success')
             else:
                 print("неверный логин или пароль")
+                self.show_error_message("Ошибка", "Неверный логин или пароль")
 
         except sq.Error as e:
             print(e)
@@ -60,6 +62,10 @@ class Application(tk.Tk):
 
     def show_main_window(self):
         self.deiconify()  # Восстанавливаем главное окно
+
+    def show_error_message(self, title, message):
+        messagebox.showerror(title, message)
+
 
 class WindowAuthorization(tk.Toplevel):
     def __init__(self, master=None):
@@ -78,7 +84,8 @@ class WindowAuthorization(tk.Toplevel):
 
         self.status_label = tk.Label(self, text="Статус:")
         self.status_label.pack(anchor=tk.CENTER)
-        self.status_choice = ttk.Combobox(self, values=("Аббитуриент", "Секретарь", "Служащий приёмной комиссии", "Администратор"), state="readonly")
+        self.status_choice = ttk.Combobox(self, values=(
+        "Аббитуриент", "Секретарь", "Служащий приёмной комиссии", "Администратор"), state="readonly")
         self.status_choice.pack(anchor=tk.CENTER)
 
         self.login_label = tk.Label(self, text="Логин:")
@@ -91,22 +98,20 @@ class WindowAuthorization(tk.Toplevel):
         self.password_entry = tk.Entry(self)
         self.password_entry.pack(anchor=tk.CENTER)
 
-
         self.button_authorization = tk.Button(self, text="Зарегистрироваться", font="Consolas 15", relief=tk.GROOVE,
                                               bd=5,
                                               bg="#B3E5FC", fg="white",
                                               activebackground="#B3E5FC", activeforeground="white",
                                               command=self.register_and_return)
         self.button_authorization.pack(anchor=tk.CENTER, pady=30)
+        self.button_exit = tk.Button(self, text="Выйти", font="Consolas 15", command=self.exit_application)
+        self.button_exit.pack(anchor=tk.S, pady=50)
 
+    def registration(self, name, surname, status, login, password):
 
-    def registration(self):
-        name = self.name_entry.get()
-        surname = self.surname_entry.get()
-        fullname = name+" "+ surname
-        status = self.status_choice.get()
-        login = self.login_entry.get()
-        password = self.password_entry.get()
+        if not (name and surname and status and login and password):
+            messagebox.showerror("Ошибка", "Заполните все поля")
+            return
         try:
             db = sq.connect('../database/priemka.db')
             cursor = db.cursor()
@@ -114,12 +119,16 @@ class WindowAuthorization(tk.Toplevel):
             SELECT login FROM users WHERE login = ?
             """, [login])
             if cursor.fetchone() is None:
-                cursor.execute("INSERT INTO users(login, fullname, status, password) VALUES (?,?,?,?)", [login, fullname,status, password])
+                cursor.execute("INSERT INTO users(login, fullname, status, password) VALUES (?,?,?,?)",
+                               [login, name + " " + surname, status, password])
                 db.commit()
+                self.withdraw()
+                appli = Application()
+                appli.deiconify()
             else:
-                self.login_entry.config(bg="red")
-                messagebox.showerror(text="Такой логин уже существует", fg="red")
-                self.registration()
+                self.login_entry.delete(0, tk.END)
+                messagebox.showerror("Ошибка", "Такой логин уже существует")
+
         except sq.Error as e:
             print(e)
         finally:
@@ -127,9 +136,17 @@ class WindowAuthorization(tk.Toplevel):
             db.close()
 
     def register_and_return(self):
-        self.registration()  # Регистрируем пользователя
+        name = self.name_entry.get()
+        surname = self.surname_entry.get()
+        status = self.status_choice.get()
+        login = self.login_entry.get()
+        password = self.password_entry.get()
+        self.registration(name, surname, status, login, password)
+
+    def exit_application(self):
         self.destroy()
-        self.master.show_main_window()
+
+
 
 if __name__ == "__main__":
     data = []  # Данные, собранные при входе
